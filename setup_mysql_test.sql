cdc812aab39fa6b770355254b47b0bc055e90b7b
Katekani Madingana <madinganakn@gmail.com>
2023-08-24 02:56:25 -0700

    test database

---
+++ setup_mysql_test.sql
@@ -0,0 +1,15 @@
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant all privileges to hbnb_dev on hbnb_dev_db
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege to hbnb_dev on performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
+
