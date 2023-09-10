15e1d8370d7ed09b28d28829480c72a6dcff0629
Katekani Madingana <madinganakn@gmail.com>
2023-09-10 07:51:30 -0700

    last try

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
 """
-packege all files as tgz
-"""
+This script defines a Fabric task that packages the contents of the web_static
+folder into a .tgz archive and stores it in the 'versions' directory.
 
+The generated archive is named 'web_static_<year><month><day><hour><minute><second>.tgz'.
+"""
 
 from fabric.api import *
 import time
 
-
 def do_pack():
     """
-    function that packages folder
+    Create a compressed archive from the contents of the web_static folder.
 
     Returns:
-        Archive path or None if not successful
+        str: Archive path if successful, None if the archive wasn't created.
     """
     strtime = time.strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
         file_name = "web_static_{}.tgz".format(strtime)
         local("tar -cvzf versions/{} web_static".format(file_name))
-        return("versions/{}".format(file_name))
-    except as e:
+        return "versions/{}".format(file_name)
+    except Exception as e:
         return None
+
